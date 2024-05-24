40dfbf5f1594887609f1f873b62c26b1ff87c58f
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-05-24 18:48:04 -0400

    board and handlers ready

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -2,12 +2,33 @@ import React, { useState } from "react";
 import "./ticTacToe.css";
 
 const TicTacToe = () => {
+  const [displayMessage, setDisplayMessage] = useState("Current Player: X");
   const [board, setBoard] = useState(new Array(9).fill(null));
+
+  const handleReset = () => {
+    console.log("handleReset");
+  };
+
+  const handleXO = (ind) => {
+    console.log("handleXO", ind);
+  };
+
   return (
     <div className="game">
       <div><h1>Tic Tac Toe</h1></div>
+      <div className="displayMessage">{displayMessage}</div>
+      <div>
+        {board.every((cell) => cell !== null) ? (
+          <button className="reset-button" onClick={handleReset}>Reset</button>
+        ) : null}
+      </div>
+      <div className="board">
+        {board.map((xo, ind) => (
+          <div className="cell" key={ind} value={xo} onClick={() => handleXO(ind)}>{xo}</div>
+        ))}
+      </div>
     </div>
-  )
+  );
 };
 
 export default TicTacToe;
