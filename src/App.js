7cd7e5b89e8604995ee92272a5ee2a7989552827
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-05-24 18:35:30 -0400

    Ready for TicTacToe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import Selector from "./components/Selector/Selector";
 import Countdown from "./components/CountdownTimer/Countdown";
+import TicTacToe from "./components/TicTacToe/TicTacToe";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       {/* <Selector /> */}
-      <Countdown />
+      {/* <Countdown /> */}
+      <TicTacToe />
     </div>
   );
 }
