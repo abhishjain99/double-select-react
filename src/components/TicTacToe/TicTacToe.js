2d79f95ea4680ac5323932dab53dab2469078b5c
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-05-24 19:09:27 -0400

    logic for winner

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import "./ticTacToe.css";
+
+const getWinner = (board) => {
+  const winnerIn = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6],
+  ];
+  for (let [f, s, t] of winnerIn) {
+    if (board[f] === board[s] && board[s] === board[t] && board[f] !== null) return board[f];
+  }
+  return null;
+};
 
 const TicTacToe = () => {
   const [displayMessage, setDisplayMessage] = useState("Current Player: X");
   const [board, setBoard] = useState(new Array(9).fill(null));
+  const winner = useMemo(() => getWinner(board), [board]);
 
   const handleReset = () => {
     console.log("handleReset");
