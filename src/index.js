6a49de43167fd72c923904d4de69a5bccc53fb83
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-06-04 21:13:30 -0400

    store updated for react-typescript in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './components/FunctionalComponent_RTK/todoStore';
+// import store from './components/FunctionalComponent_RTK/todoStore';
+import store from './components/Typescript_FuncComp_RTK/todoStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
