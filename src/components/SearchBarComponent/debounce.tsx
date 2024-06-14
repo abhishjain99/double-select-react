75f52e042821f5f4125c1b1a75a2521578092996
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-06-14 15:22:35 -0400

    Handled fallback for data when empty list is sent by server

--- src/components/SearchBarComponent/debounce.tsx
+++ src/components/SearchBarComponent/debounce.tsx
@@ -8,10 +8,10 @@ const fetchDropdownContent = (setDropdown: any) => debounce(async (searchedText:
         const response = await axios.get(
           `https://www.googleapis.com/books/v1/volumes?q=${searchedText}&startIndex=0&maxResults=20`
         );
-        const books = response.data.items.map((item: any) => ({
+        const books = response.data.items?.map((item: any) => ({
           id: item.id,
           title: item.volumeInfo.title,
-        }));
+        })) ?? []; // fallback when the book is not found for the data entered in searchbar
         setDropdown(books);
       } catch (error) {
         console.error("Error fetching dropdown content:", error);
